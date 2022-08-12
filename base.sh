5af9df4239c2ebb9919b46d3b93b9155b4dd10f5
Elitornyx <98707728+Elitornyx@users.noreply.github.com>
2022-08-13 01:37:12 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -18,17 +18,18 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ASUSLaptop-N580GD.localdomain ASUSLaptop-N580GD" >> /etc/hosts
 
-#pacman -S btrfs-progs intel-ucode networkmanager network-manager-applet sudo grub efibootmgr os-prober nano
+pacman -S btrfs-progs intel-ucode networkmanager sudo grub efibootmgr os-prober nano ntfs-3g
 
 
 
 
 
-# xdg-user-dirs xdg-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call tlp ntfs-3g reflector
+# xdg-user-dirs xdg-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call tlp reflector
 
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings  nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-opencl-nvidia opencl-nvidia libxnvctrl  
 # pacman -Syu lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader libva-intel-driver xf86-video-intel
 
+mkdir /boot/efi
 mount /dev/sda1 /boot/efi/
 grub-install --target=x86_64-efi --efi-directory=/boot/efi/ --bootloader-id=ArchLinux --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
