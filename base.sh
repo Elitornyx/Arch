84f6b11e7cba88bcfe243471bbb733bc3666071f
Elitornyx <98707728+Elitornyx@users.noreply.github.com>
2022-08-13 02:37:47 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -13,12 +13,12 @@ echo "LC_COLLATE=C" >> /etc/locale.conf
 echo "KEYMAP=ru" >> /etc/vconsole.conf
 echo "FONT=cyr-sun16" >> /etc/vconsole.conf
 
-echo "ASUSLaptop-N580GD" >> /etc/hostname
+echo "Arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 ASUSLaptop-N580GD.localdomain ASUSLaptop-N580GD" >> /etc/hosts
+echo "127.0.1.1 Arch.localdomain Arch" >> /etc/hosts
 
-pacman -S btrfs-progs intel-ucode networkmanager sudo grub efibootmgr os-prober nano ntfs-3g
+pacman -S btrfs-progs intel-ucode networkmanager sudo grub efibootmgr nano ntfs-3g
 
 mkdir /boot/efi
 mount /dev/vda1 /boot/efi/
@@ -26,9 +26,5 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi/ --bootloader-id=Arch
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
-
-useradd -m elitornyx
-echo elitornyx:password | chpasswd
-usermod -aG libvirt elitornyx
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
