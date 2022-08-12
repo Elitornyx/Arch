f1e9eb6d0d74401ce83ba13b4fd460ba060fee71
Elitornyx <98707728+Elitornyx@users.noreply.github.com>
2022-08-13 02:14:48 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -20,17 +20,8 @@ echo "127.0.1.1 ASUSLaptop-N580GD.localdomain ASUSLaptop-N580GD" >> /etc/hosts
 
 pacman -S btrfs-progs intel-ucode networkmanager sudo grub efibootmgr os-prober nano ntfs-3g
 
-
-
-
-
-# xdg-user-dirs xdg-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call tlp reflector
-
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings  nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-opencl-nvidia opencl-nvidia libxnvctrl  
-# pacman -Syu lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader libva-intel-driver xf86-video-intel
-
 mkdir /boot/efi
-mount /dev/sda1 /boot/efi/
+mount /dev/vda1 /boot/efi/
 grub-install --target=x86_64-efi --efi-directory=/boot/efi/ --bootloader-id=ArchLinux --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -39,8 +30,5 @@ systemctl enable NetworkManager
 useradd -m elitornyx
 echo elitornyx:password | chpasswd
 usermod -aG libvirt elitornyx
-
-echo "elitornyx ALL=(ALL) ALL" >> /etc/sudoers.d/elitornyx
-
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
